4f38e18e6352b82fdf42444ed8977c7d7b2f675e
José María Mateos <chema@rinzewind.org>
2012-05-27 05:19:55 -0600

    Update time will depend on each particular execution. General message removed

--- kiva_secular.py
+++ kiva_secular.py
@@ -106,8 +106,6 @@ def main():
         <p>You can find the code for the script used to generate this web page 
         <a href="https://github.com/rinze/kiva_secular/">in Github</a>.</p>
 
-        <p>This page is updated every 5 hours. Enjoy.</p>
-        
         <div id="loanlist">
     """
     for p in approved_projects:
